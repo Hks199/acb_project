2dffed3846a90bfcc87d29693472ed93417f9095
Hks199 <97798485+Hks199@users.noreply.github.com>
2025-07-22 22:52:53 +0530

    changes

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-// User Schema
+// User Schema  
 const userSchema = new mongoose.Schema(
   {
     first_name: {
